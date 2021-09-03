e05d8a21d3b88c3b6b95f071c12d7b69750815f0
Mark C <mark@dcode.com.au>
2021-09-03 15:18:52 +1000

    updates to sass and stylegudie blades

--- README.md
+++ README.md
@@ -1,24 +1,4 @@
-
-## Installing DPres  
-In order to run the Dcode preset we need to include the Laravel Ui package:
-```composer require laravel/ui```
-ToDo: Lets look for other options instead of laravel/ui. This is a not a recommended package.
-
-Include a namespace reference to the Dcode Preset from the root composer.json 
-```
-"autoload-dev": {
-    "psr-4": {
-        "Dcode\\UiPresets\\": "ui-presets/dcode/src/"
-    }
-},
-```
-Include a reference to the DcodeServiceProvider in the app Service Provider class.
-``` Dcode\UiPresets\DcodeServiceProvider::class, ```
-
 ## Running Preset
-Refresh the composer autoloaded classes:
-```composer dump-autoload```
-
 To build the preset boilerplate, run the following command:
 ```php artisan ui dpres```
 
--- stubs/default/resources/views/styleguide/layouts/flex.blade.php
+++ stubs/default/resources/views/styleguide/layouts/flex.blade.php
@@ -8,24 +8,25 @@
 
 <div class="content">
     <h3>About flex cols</h3>
-    <p>A mixin for generating the flex columns is available. <code>@mixin flex-cols($breakpoint: xs);</code></p>
-    <p>By defining the columns in the $columns map, we can create flex layouts which are defined by the ammount of columns set.</p>
-    <p>The key values match up with the <a href="{{ route('styleguide.layouts.breakpoints') }}">breakpoints map</a> to define the layout depending on the breakpoint width.</p>
+    <p>A helper mixin for generating the flex columns is available called <code>flex-cols($breakpoint: xs);</code></p>
+    <p>By defining the columns in a $columns map, we can create flex layouts with predefined columns.</p>
+    <p>The map's key values, match up with the <a href="{{ route('styleguide.layouts.breakpoints') }}">breakpoints map</a> and define the number of columns for those breakpoints.</p>
     <div class="demo">
         <pre>
             <code>
 {{ "
 \$columns: (
-  xs: (
-    columns: 7,
-  ),
-
-  sm: (
+    xs: (
     columns: 2,
+    gutter: 16,
   ),
-
+  sm: (
+    columns: 6,
+    gutter: 12,
+  ),
   lg: (
     columns: 12,
+    gutter: 8,
   )
 ) !default;"
             }}
@@ -34,20 +35,61 @@
     </div>
     <hr>
     <h3>Demo</h3>
-    <p>The below demo is a 12 column flex column layout with a <code>.space-between</code> css class included.</p>
+    <p>The below demo is a large 12 column flex column layout with a <code>.justify-between</code> css class included.</p>
     <div class="demo">
         <div class="flex-cols justify-between">
-            <div class="lg-4 ">Large 4</div>
-            <div class="lg-2 ">Large 2</div>
-            <div class="lg-2 ">Large 2</div>
-            <div class="lg-3 ">large 3</div>
+            <div class="lg-4">Large 4</div>
+            <div class="lg-2">Large 2</div>
+             <div class="lg-3">large 3</div>
+            <div class="lg-1">large 1</div>
+
             <div class="lg-5 ">large 5</div>
             <div class="lg-6 ">large 6</div>
          </div>
     </div>
     <hr>
+    <h3>Flex col's margins & padding</h3>
+    <p>Each child element of a has padding and margin class helpers named <code>.padd</code> and <code>.mrg</code></p>
+    <p>These classes will add a left and right padding or margin value based on the gutter settings defined in the $columns map seen above.</p>
+    <hr>
+    <h3>Demo</h3>
+    <p>The below demo has both the <code>.padd</code> and the<code>.mrg</code> css classes included.</p>
+    <div class="demo">
+        <div class="flex-cols">
+            <div class="lg-4 padd mrg">Large 4</div>
+            <div class="lg-2 padd mrg">Large 2</div>
+            <div class="lg-2 padd mrg">Large 2</div>
+            <div class="lg-3 padd mrg">large 3</div>
+            <div class="lg-1 padd mrg">large 1</div>
+            <div class="lg-5 padd mrg">large 5</div>
+            <div class="lg-6 padd mrg">large 6</div>
+         </div>
+    </div>
+    <hr>
+    <h3>Code Example</h3>
+    <p>Below is the @flex-cols settings used for the demo:</p>
+    <div class="demo">
+        <pre>
+            <code>
+{{ "
+.flex-cols {
+    @'include flex-cols(lg);
+    > div {
+        background-color: \$dcode-turquoise;
+        border: 1px solid #fff;
+        color: #fff;
+        min-height: 100px;
+        // padding: 1rem;
+        margin-bottom: 1rem;
+    }
+}" }}
+            </code>
+        </pre>
+    </div>
+    <hr>
+
     <h3>Flex col alignment helpers</h3>
-    <p>There is a list of flex helpers which can be added to the flex container which are listed below:</p>
+    <p>There is a list of flex css class helpers which can be added to the flex container. These are listed below:</p>
     <div class="demo">
         <pre>
             <code>
